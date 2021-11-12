0e921cd31278d22023ab4335f861c041e4589e77
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-11-12 09:18:04 -0500

    Exercise 2 done

---
+++ nodeschool/learnyounode/cristian-moreno/2-baby-stetps.js
@@ -0,0 +1,17 @@
+const numbers = process.argv.slice(2);
+
+const suma = numbers.reduce(function(previous, current) {
+  return previous + Number(current);
+}, 0)
+
+console.log(suma)
+
+// 'use strict'
+
+// let result = 0
+
+// for (let i = 2; i < process.argv.length; i++) {
+//   result += Number(process.argv[i])
+// }
+
+// console.log(result)
