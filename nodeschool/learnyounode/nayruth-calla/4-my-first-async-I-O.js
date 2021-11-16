6ed35fba12bcac74db5c1da01ae8c193c1652577
nayruthCalla <nayruth.calla@gmail.com>
2021-11-15 23:00:39 -0500

    Exercise 4 done

---
+++ nodeschool/learnyounode/nayruth-calla/4-my-first-async-I-O.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const buf = fs.readFile(process.argv[2],(err,data)=>{
+    if(err)throw console.log(err);
+    const lines = data.toString().split('\n').length-1
+    console.log(lines);
+})
