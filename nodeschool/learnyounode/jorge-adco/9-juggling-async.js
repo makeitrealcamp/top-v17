8078f2e0499f88666825d2aef55216c163fd8c1a
Jorge Luis Adco <jorgead0812@gmail.com>
2021-11-17 14:03:52 -0500

    Ejercicio 9 Completado

---
+++ nodeschool/learnyounode/jorge-adco/9-juggling-async.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const bl = require("bl");
+
+const urls = process.argv.slice(2);
+let count = 0;
+let res = [];
+
+
+function http_res(element, index) {
+  http.get(element, function (response) {
+    response.pipe(
+      bl(function (err, data) {
+        if (err) return console.log(err);
+        res[index] = data.toString();
+        count++;
+        if (count === 3) {
+          res.forEach((el) => console.log(el));
+        }
+      })
+    );
+  });
+}
+urls.forEach((el, index) => {
+    http_res(el, index);
+  });
+  
