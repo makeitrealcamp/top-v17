6a89dbe12af655cf756c6d4a52831eb281760be9
nayruthCalla <nayruth.calla@gmail.com>
2021-11-17 18:02:04 -0500

    Exercise 11 done

---
+++ nodeschool/learnyounode/nayruth-calla/11-http-file-server.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer((req,res)=>{
+    res.writeHead(200, 'content-type: text/plain')
+    fs.createReadStream(process.argv[3]).pipe(res)
+}).listen(process.argv[2]);
