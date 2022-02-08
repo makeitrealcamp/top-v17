59ec9cc3200d9baaafc11c623420a8b54c83d4fd
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-02-08 14:47:06 -0500

    feat: RN example

---
+++ examples/rn-example/App.js
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import { View, Switch, Text, Image, TextInput, Button, Platform, Alert, TouchableOpacity } from 'react-native'
+import * as ImagePicker from 'expo-image-picker';
+
+import { styles } from './App.styles'
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [text, setText] = useState('Press me')
+  const [number, setNumber] = useState(0)
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const handleChange = (text) => {
+    setText(text)
+  }
+
+  const onChangeNumber = (num) => {
+    setNumber(num)
+  }
+
+  const pickImage = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={pickImage}
+      >
+        <Image
+          source={{
+            uri: !image
+              ?'https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg'
+              : image.uri
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleChange}
+      />
+
+      {
+        Platform.OS === 'ios'
+        ? (<TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+            keyboardAppearance="dark"
+          />)
+        : (<TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+            autoComplete='name'
+          />)
+      }
+
+
+
+      <Button
+        title="Learn More"
+        color="#841584"
+        onPress={pickImage}
+        accessibilityLabel="Learn more about this purple button"
+
+      />
+
+      {/* <Switch
+        // trackColor={{ false: "#767577", true: "#81b0ff" }}
+        // thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      /> */}
+
+      {/* <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+        style={styles.image}
+      /> */}
+    </View>
+  );
+}
+
+
+
+export default App;
---
+++ examples/rn-example/App.styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Platform } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  button: {
+    alignItems: "center",
+    // backgroundColor: "#DDDDDD",
+    padding: 10,
+  }
+})
